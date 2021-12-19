54a365c4d273c2fa4f2dbb2a213979b85b8d5c98
나미 <nami0195103@gmail.com>
2021-12-19 19:45:28 +0900

    위상정렬

--- 2021/boj/2293.cpp
+++ 2021/boj/2293.cpp
@@ -1,24 +1,20 @@
 //위상정렬
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
-#include <utility>
 using namespace std;
 
 int main() {
 	int n, m, i, j;
-	vector<bool> visited;
 	vector<vector<int>> edge;	//연결 정보
 	vector<int> ans;
 	vector<int> in_deg;	//진입차수
 	queue<int> q;
 
-	//초기화
+	//초기화 - DAG 아닌 그래프 확인해야 할 수도 있음
 	scanf("%d %d", &n, &m);
 	edge.assign(n + 1, vector<int>(0));
 	in_deg.assign(n + 1, 0);
-	visited.assign(n + 1, false);
 	for (i = 0; i < m; i++) {
 		int k;
 		scanf("%d %d", &j, &k);
@@ -32,15 +28,13 @@ int main() {
 			q.push(i);
 	}
 
-	//위상정렬 - queue가 비었는데 방문하지 않은 정점이 있다면 사이클 존재
+	//위상정렬
 	while (!q.empty()) {
 		i = q.front();
 		q.pop();
 		ans.push_back(i);
-		visited[i] = true;
 		for (auto iter : edge[i]) {
-			in_deg[iter]--;
-			if (!in_deg[iter] && !visited[iter])
+			if (--in_deg[iter] == 0)
 				q.push(iter);
 		}
 	}
